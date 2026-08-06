cd496cdc412a281eb7e9a815604667275df4d141
maxjohn81 <rivoandriharisoa@gmail.com>
2026-08-06 11:45:35 +0300

    Redeploy pour variables env

--- api/stats.js
+++ api/stats.js
@@ -1,6 +1,9 @@
 import { Redis } from "@upstash/redis";
 
-const redis = Redis.fromEnv();
+const redis = new Redis({
+  url: process.env.KV_REST_API_URL || process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.KV_REST_API_TOKEN || process.env.UPSTASH_REDIS_REST_TOKEN,
+});
 
 export default async function handler(req, res) {
   const secret = process.env.STATS_SECRET;
