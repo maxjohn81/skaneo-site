609f94f9bca9a2cdac5e67f0975289f15e5a67b2
maxjohn81 <rivoandriharisoa@gmail.com>
2026-09-12 17:56:19 +0300

    Version notification

---
+++ api/version.js
@@ -0,0 +1,10 @@
+export default function handler(_req, res) {
+  res.status(200).json({
+    version: "1.0.2",
+    url: "https://skaneo.vercel.app/api/download",
+    notes:
+      "Nouveau : notification push pour les nouvelles versions de l'application.\n\n" +
+      "Correction : correction d'un bug qui empêchait la détection de texte sur certaines images.\n\n" +
+      "Amélioration : amélioration de la vitesse de traitement des images pour une meilleure expérience utilisateur.",
+  });
+}
